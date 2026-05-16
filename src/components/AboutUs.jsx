5752669904c38bac57a6f663e60aec6d3ed11296
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 12:11:20 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,1 +1,12 @@
-Coming soon
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shop offering beautiful indoor and outdoor plants.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
