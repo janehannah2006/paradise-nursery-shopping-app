cee811289183ec0af610b540282c516799b4d38a
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 12:20:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import ProductList from "./components/ProductList";
+
 function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
     <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+      {!showProductList ? (
+        <div>
+          <h1>Welcome to Paradise Nursery</h1>
+
+          <button onClick={() => setShowProductList(true)}>
+            Get Started
+          </button>
+        </div>
+      ) : (
+        <ProductList />
+      )}
     </div>
   );
 }
