5b055ddc187040b5b4f8053909e4f0ebe34fbdb7
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 12:12:30 +0530

    Update CartSlice.jsx

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -1,1 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  cartItems: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.cartItems.push(action.payload);
+      state.totalQuantity += 1;
+    },
+
+    removeItem: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        item => item.id !== action.payload
+      );
+    },
+
+    increaseQuantity: (state, action) => {
+      state.totalQuantity += 1;
+    },
+
+    decreaseQuantity: (state, action) => {
+      state.totalQuantity -= 1;
+    },
+  },
+});
+
+export const {
+  addItem,
+  removeItem,
+  increaseQuantity,
+  decreaseQuantity,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
