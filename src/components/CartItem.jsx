86111aba1650ebef44b1d158435740f497ce52d5
janehannah2006 <727824tuam018@skct.edu.in>
2026-05-16 12:13:31 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,1 +1,32 @@
+function CartItem() {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
 
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6"
+          alt="plant"
+          width="100"
+        />
+
+        <h3>Aloe Vera</h3>
+        <p>Unit Price: $10</p>
+        <p>Total Cost: $20</p>
+
+        <button>+</button>
+        <button>-</button>
+        <button>Delete</button>
+      </div>
+
+      <h2>Total Cart Amount: $20</h2>
+
+      <button>Checkout</button>
+      <p>Coming Soon</p>
+
+      <button>Continue Shopping</button>
+    </div>
+  );
+}
+
+export default CartItem;
